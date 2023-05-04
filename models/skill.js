603c51f90f33a45ab4f33623237ee45258908e0b
danielamansolino <danielamosqueragranda@gmail.com>
2023-05-03 21:14:26 -0400

    Express "Dev Skills" Part 2 complete - Bonus Exercises complete - Bootstrap complete

--- models/skill.js
+++ models/skill.js
@@ -28,8 +28,29 @@ function getOne(id) {
     return skills.find(skill => skill.id === id)
 }
 
+function create(skill) {
+    skill.id = Date.now() % 1000000
+    skill.done = false
+    skills.push(skill)
+}
+
+function deleteOne(id) {
+    id = parseInt(id)
+    const idx =skills.find(skill => skill.id ===id)
+    skills.splice(idx, 1)
+}
+
+function update(id, updateSkill) {
+    id = parseInt(id)
+    const skill = skills.find(skill => skill.id === id)
+    Object.assign(skill, updateSkill)
+}
+
 //exporting to use elsewhere in my app
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
 }
--- routes/skills.js
+++ routes/skills.js
@@ -7,11 +7,21 @@ const skillController = require('../controllers/skills')
 // GET request localhost:3000/todo
 router.get('/', skillController.index)
 
+// GET Request to render a form localhost:3000/skills/new
+router.get('/new', skillController.newSkill)
+
 // GET request localhot:3000/todos/125223
 // Since this is a GET request we need to get the user id somehow. This is with a url parameter.
 // W can name ':id' whatever we want just a long as we then refer to it as what we named it 
 router.get('/:id', skillController.show)
 
+router.post('/', skillController.create)
+
+router.delete('/:id', skillController.deleteSkill)
+
+router.get('/:id/edit', skillController.edit)
+
+router.put('/:id', skillController.update)
 
 // If there is only one export you don't need the object
 module.exports = router;
